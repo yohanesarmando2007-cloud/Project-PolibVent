91451b0838065385f77d9dac830894573e5ba6dd
yohan <yohanesarmando2007@gmail.com>
2025-10-19 09:11:33 +0700

    menjadikan satu folder

---
+++ PolibVent/js/script_umum.js
@@ -0,0 +1,17 @@
+//menu active
+
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(".menu");
+
+menuBar.addEventListener('click', () => {
+    menuNav.classList.toggle('menu-active');
+});
+
+//scrolling active
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    const windowPosition = window.scrollY > 0;
+    navBar.classList.toggle("scrolling-active",windowPosition);
+});
